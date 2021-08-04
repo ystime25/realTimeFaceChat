a7c119f72c10653d79d613d64d38229492698655
ystime25 <ystime25@gmail.com>
2021-08-04 14:28:43 +0900

    mute and camera Btn config

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,18 +4,35 @@ const socket = io();
 const userFace = document.getElementById("userFace");
 const muteBtn = document.getElementById("muteBtn");
 const cameraBtn = document.getElementById("cameraBtn");
+const selectCamera = document.getElementById("cameras");
 
 let userStream;
 let mute = false;
 let cameraOff = false;
 
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      selectCamera.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 const getMedia = async () => {
   try {
     userStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
+      audio: true,
       video: true,
     });
     userFace.srcObject = userStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
@@ -24,6 +41,9 @@ const getMedia = async () => {
 getMedia();
 
 const handleMuteClick = () => {
+  userStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!mute) {
     muteBtn.innerText = "음소거 해제";
     mute = true;
@@ -34,11 +54,14 @@ const handleMuteClick = () => {
 };
 
 const handleCameraClick = () => {
+  userStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "카메라 끄기";
     cameraOff = false;
   } else {
-    cameraBtn.innerText = "카메라 키기";
+    cameraBtn.innerText = "카메라 켜기";
     cameraOff = true;
   }
 };
